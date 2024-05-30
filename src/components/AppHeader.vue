22f709a83ec24460d55c66e409a898db3ca0e0a6
michelealiffi <michelealiffi890@gmail.com>
2024-05-30 14:10:09 +0200

    First commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            logo: 'assets/boolean-logo.png',
+        }
+    },
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+              <span class="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+          </nav>
+    </header>
+</template>
